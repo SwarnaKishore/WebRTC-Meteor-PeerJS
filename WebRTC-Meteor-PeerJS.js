54668d977f730683e77cff81f0fd1dc21f4e1488
Swarna Kishore <swarnakishoree@gmail.com>
2015-08-08 09:36:41 -0500

    added user accounts

--- WebRTC-Meteor-PeerJS.js
+++ WebRTC-Meteor-PeerJS.js
@@ -1,8 +1,34 @@
-if (Meteor.isClient) {  
+if (Meteor.isClient) {
+  Template.hello.events({
+    "click #makeCall": function () {
+      var user = this;
+      var outgoingCall = peer.call(user.profile.peerId, window.localStream);
+      window.currentCall = outgoingCall;
+      outgoingCall.on('stream', function (remoteStream) {
+        window.remoteStream = remoteStream;
+        var video = document.getElementById("theirVideo")
+        video.src = URL.createObjectURL(remoteStream);
+      });
+    },
+    "click #endCall": function () {
+      window.currentCall.close();
+    }
+  });
+
+  Template.hello.helpers({
+    users: function () {
+      // exclude the currentUser
+      var userIds = Presences.find().map(function(presence) {return presence.userId;});
+      return Meteor.users.find({_id: {$in: userIds, $ne: Meteor.userId()}});
+    }
+  });
+
   Template.hello.onCreated(function () {
-      // Create a Peer instance
-      window.peer = new Peer({
-      key: 'vw9xr26goox8yqfr',  // get a free key at http://peerjs.com/peerserver
+    Meteor.subscribe("presences");
+    Meteor.subscribe("users");
+
+    window.peer = new Peer({
+      key: '2p9ffp7ol6p3nmi',  // change this key
       debug: 3,
       config: {'iceServers': [
         { url: 'stun:stun.l.google.com:19302' },
@@ -10,12 +36,18 @@ if (Meteor.isClient) {
       ]}
     });
 
-    // Handle event: upon opening our connection to the PeerJS server
+    // This event: remote peer receives a call
     peer.on('open', function () {
       $('#myPeerId').text(peer.id);
+      // update the current user's profile
+      Meteor.users.update({_id: Meteor.userId()}, {
+        $set: {
+          profile: { peerId: peer.id}
+        }
+      });
     });
 
-    // Handle event: remote peer receives a call
+    // This event: remote peer receives a call
     peer.on('call', function (incomingCall) {
       window.currentCall = incomingCall;
       incomingCall.answer(window.localStream);
@@ -25,12 +57,30 @@ if (Meteor.isClient) {
         video.src = URL.createObjectURL(remoteStream);
       });
     });
+
+    navigator.getUserMedia = ( navigator.getUserMedia ||
+                      navigator.webkitGetUserMedia ||
+                      navigator.mozGetUserMedia ||
+                      navigator.msGetUserMedia );
+
+    // get audio/video
+    navigator.getUserMedia({audio:true, video: true}, function (stream) {
+      //display video
+      var video = document.getElementById("myVideo");
+      video.src = URL.createObjectURL(stream);
+      window.localStream = stream;
+    }, function (error) { console.log(error); }
+    );
+
   });
+
 }
 
-
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Meteor.publish('presences', function() {
+    return Presences.find({}, { userId: true });
+  });
+  Meteor.publish("users", function () {
+    return Meteor.users.find({}, {fields: {"profile.peerId": true, "emails.address": true} });
   });
 }
