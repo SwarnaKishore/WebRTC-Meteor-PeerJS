cd7cf28404db3d521afe2cb979ac7ea1d715d60f
Swarna Kishore <swarnakishoree@gmail.com>
2015-08-07 21:22:08 -0500

    added templates

--- WebRTC-Meteor-PeerJS.js
+++ WebRTC-Meteor-PeerJS.js
@@ -1,20 +1,33 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+if (Meteor.isClient) {  
+  Template.hello.onCreated(function () {
+      // Create a Peer instance
+      window.peer = new Peer({
+      key: 'vw9xr26goox8yqfr',  // get a free key at http://peerjs.com/peerserver
+      debug: 3,
+      config: {'iceServers': [
+        { url: 'stun:stun.l.google.com:19302' },
+        { url: 'stun:stun1.l.google.com:19302' },
+      ]}
+    });
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+    // Handle event: upon opening our connection to the PeerJS server
+    peer.on('open', function () {
+      $('#myPeerId').text(peer.id);
+    });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
+    // Handle event: remote peer receives a call
+    peer.on('call', function (incomingCall) {
+      window.currentCall = incomingCall;
+      incomingCall.answer(window.localStream);
+      incomingCall.on('stream', function (remoteStream) {
+        window.remoteStream = remoteStream;
+        var video = document.getElementById("theirVideo")
+        video.src = URL.createObjectURL(remoteStream);
+      });
+    });
   });
 }
+
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
